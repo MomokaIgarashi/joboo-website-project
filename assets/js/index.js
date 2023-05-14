655add5cbe09966367f503d48e9e6b4b48a1f2a0
Momoka Igarashi <migarashi0318@gmail.com>
2023-05-14 21:17:36 +0200

    new job section revise

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,4 +20,24 @@ $(document).ready(function() {
     });
 })();
 
+// Initialize Swiper
+// var swiper = new Swiper('.swiper', {
+//     slidesPerView: 3,
+//     direction: getDirection(),
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     on: {
+//         resize: function () {
+//         swiper.changeDirection(getDirection());
+//         },
+//     },
+// });
 
+// function getDirection() {
+//     var windowWidth = window.innerWidth;
+//     var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+//     return direction;
+// }
