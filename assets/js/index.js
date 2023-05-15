e1164b503129b17b4ea88e1c0f0d22e46acd61ea
Momoka Igarashi <migarashi0318@gmail.com>
2023-05-15 19:29:01 +0200

    code delete for comment out

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,25 +19,3 @@ $(document).ready(function() {
         }
     });
 })();
-
-// Initialize Swiper
-// var swiper = new Swiper('.swiper', {
-//     slidesPerView: 3,
-//     direction: getDirection(),
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     on: {
-//         resize: function () {
-//         swiper.changeDirection(getDirection());
-//         },
-//     },
-// });
-
-// function getDirection() {
-//     var windowWidth = window.innerWidth;
-//     var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-//     return direction;
-// }
