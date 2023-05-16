6152ca524791be334a26cf34f6bbcdb8c11dfac0
Momoka Igarashi <migarashi0318@gmail.com>
2023-05-16 07:18:08 +0200

    header button revise

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,22 @@ $(document).ready(function() {
 });
 
 // Audio loop play
-
 (() => {
     let ad = new Audio("assets/images/joboo-jingle.mp3");
     ad.loop = true;
 
     const element = document.getElementById('loop');
     element.addEventListener('click', function() {
+        if (ad.paused) {
+            ad.play()
+        }
+        else{
+            ad.pause()
+        }
+    });
+
+    const elementsp = document.getElementById('loop--sp');
+    elementsp.addEventListener('click', function() {
         if (ad.paused) {
             ad.play()
         }
